0e14274292fcd7724983475871db76cfdb125f09
echerkas <dark_lady_666@mail.ru>
2015-12-03 00:31:53 +0300

    all

---
+++ src/main/com.personal.cabinet/Main.java
@@ -0,0 +1,11 @@
+package com.personal.cabinet;
+
+/**
+ * Created by echerkas on 03.12.2015.
+ */
+public class Main {
+    int x;
+    public static void main (String [] args){
+        System.out.println("Hello, Stan!");
+    }
+}
