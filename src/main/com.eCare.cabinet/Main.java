923aad1c886edab740c72e83c49861b15c646c68
echerkas <dark_lady_666@mail.ru>
2015-12-03 00:21:08 +0300

    test

---
+++ src/main/com.eCare.cabinet/Main.java
@@ -0,0 +1,10 @@
+package com.eCare.cabinet;
+
+/**
+ * Created by echerkas on 03.12.2015.
+ */
+public class Main {
+    public static void main (String [] args){
+        System.out.println("Hello, Maven!");
+    }
+}
